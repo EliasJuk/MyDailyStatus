53aa3c22e008a7d4aa9fcdfc9c0b856075290c08
eliasjuk <eliasp.juk@hotmail.com>
2020-04-10 11:29:33 -0300

    :sparkles: Login and Logout

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,7 +4,9 @@ import Header from '../components/Header'
 const Index = () => {
     return (
         <div>
-            Index
+            <h1>HOME</h1>
+
+            <a href='/api/login' className="py-4 px-2 rounded bg-pink-800 font-bold shadow-lg hover:shadow-xl block w-1/4 text-center mx-auto text-white">Comece por aqui</a>
         </div>
     )
 }
