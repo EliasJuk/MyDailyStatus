7a7f2298cb10ac7b05b598ba3c42a70f49637d1c
eliasjuk <eliasp.juk@hotmail.com>
2020-04-11 15:56:26 -0300

    :dizzy: Salvar dados

--- web/pages/app.js
+++ web/pages/app.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
 import auth0 from '../lib/auth0'
 import router from 'next/router'
+import { db } from '../lib/db'
 
 const App = props => {
     useEffect(() => {
         if(!props.isAuth){
             router.push('/')
+        }else if(props.forceCreate){
+            router.push('/create-status')
         }
     })
-    if(!props.isAuth){
+    if(!props.isAuth || props.forceCreate){
         return null
     }
     return (
@@ -24,11 +27,30 @@ export default App
 export async function getServerSideProps({ req, res }) {
     const session = await auth0.getSession(req)   
     if(session){
+        const today = new Date()
+        const currentDate = 
+            today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate()
+
+        const todaysChecking = await db
+            .collection('markers')
+            .doc(currentDate)
+            .collection('checks')
+            .doc(session.user.sub)
+            .get()
+
+        const todaysData = todaysChecking.data()
+        let forceCreate = true
+        if(todaysData){
+            // pode ver os outros checkins
+            forceCreate = false
+        }     
+
         return {
             props: {
                 //isAuth: !!session.user,
                 isAuth: true,
-                user: session.user
+                user: session.user,
+                forceCreate
             }
         }
     }
