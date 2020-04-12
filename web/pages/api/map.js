1d2acdb81b5f88282db12332c943fca4773d8279
eliasjuk <eliasp.juk@hotmail.com>
2020-04-12 17:32:04 -0300

    :sparkles: Update key

--- web/pages/api/map.js
+++ web/pages/api/map.js
@@ -17,7 +17,10 @@ class SimpleMap extends Component {
         // Important! Always set the container height explicitly
         <div style={{ height: '100vh', width: '100%' }}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+                bootstrapURLKeys={{ 
+                    key: 'my key',
+                    language: 'en'
+                 }}  /*YOUR KEY HERE */
                 defaultCenter={this.props.center}
                 defaultZoom={this.props.zoom}
             >
